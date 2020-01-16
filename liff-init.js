f7f9ec264878e2df42c59a3532f60f409890397d
iskhakmuhamad <iskhakmuhama@gmail.com>
2020-01-16 17:36:59 +0700

    add profile

--- liff-init.js
+++ liff-init.js
@@ -22,16 +22,16 @@ function initiateLiff(myliffId) {
 function initializeApp() {
     document.getElementById("islogin").textContent = liff.isLoggedIn();
     document.getElementById("isclient").textContent = liff.isInClient();
-    // if (!liff.isLoggedIn()) {
-    //     document.getElementById("login").addEventListener('click', function () {
-    //         liff.login();
-    //     })
-    // } else {
-    //     document.getElementById("logout").addEventListener('click', function () {
-    //         liff.logout();
-    //         window.location.reload();
-    //     })
-    // }
+    if (!liff.isLoggedIn()) {
+        document.getElementById("login").addEventListener('click', function () {
+            liff.login();
+        })
+    } else {
+        document.getElementById("logout").addEventListener('click', function () {
+            liff.logout();
+            window.location.reload();
+        })
+    }
     document.getElementById("login").addEventListener('click', function () {
         liff.login();
     })
@@ -39,4 +39,26 @@ function initializeApp() {
         liff.logout();
         window.location.reload();
     })
+}
+
+function showProfile() {
+    document.getElementById("getprofile").addEventListener('click',function(){
+        liff.getProfile().then(function(profile) {
+            document.getElementById("userId").textContent = profile.userId;
+            document.getElementById("displayName").textContent = profile.displayName;
+            
+            const profileImage = document.getElementById("profileImage");
+            if(profileImage.firstElementChild){
+                profileImage.remove(profileImage.firstElementChild);
+            }
+            const img =document.createElement('img');
+            img.src = profile.pictureUrl;
+            img.alt = "Profile Image";
+
+            profileImage.appendChild(img);
+            document.getElementById("statusMessage").textContent = profile.statusMessage;
+        }).catch(function(err) {
+            window.alert("Gagal Mendapatkan info profile " + err);
+        })
+    });
 }
