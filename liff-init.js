63c35473a9905dc0decc90485581cb434b1daf55
iskhakmuhamad <iskhakmuhama@gmail.com>
2020-01-19 18:55:58 +0700

    aaa

--- liff-init.js
+++ liff-init.js
@@ -20,24 +20,33 @@ function initiateLiff(myliffId) {
 }
 
 function initializeApp() {
+
     document.getElementById("islogin").textContent = liff.isLoggedIn();
     document.getElementById("isclient").textContent = liff.isInClient();
+
     if (!liff.isLoggedIn()) {
         document.getElementById("login").addEventListener('click', function () {
             liff.login();
+        })
+        document.getElementById("logout").disabled = true;
+
+        document.getElementById("getprofile").addEventListener('click', function(){
+            liff.getProfile().then(function (profile) {
+            document.getElementById("userId").textContent = profile.userId;
+            document.getElementById("displayName").textContent = profile.displayName;
+            document.getElementById("statusMessage").textContent = profile.statusMessage;
+        })}).catch(function (err) {
+            window.alert("Gagal Mendapatkan info profile " + err);
         })
     } else {
         document.getElementById("logout").addEventListener('click', function () {
             liff.logout();
             window.location.reload();
         })
+        document.getElementById("login").disabled = true;
+        document.getElementById("getprofile").addEventListener('click', function(){
+            alert("Silahkan Login Terlebih dahulu");
+        })
+        
     }
-    document.getElementById("getprofile").addEventListener('click', function(){
-        liff.getProfile().then(function (profile) {
-        document.getElementById("userId").textContent = profile.userId;
-        document.getElementById("displayName").textContent = profile.displayName;
-        document.getElementById("statusMessage").textContent = profile.statusMessage;
-    })}).catch(function (err) {
-        window.alert("Gagal Mendapatkan info profile " + err);
-    })
 }
