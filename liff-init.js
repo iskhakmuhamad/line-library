2fee8c62de35b573d0173a2e0207d0773439b834
iskhakmuhamad <iskhakmuhama@gmail.com>
2020-01-19 22:13:51 +0700

    fa

--- liff-init.js
+++ liff-init.js
@@ -22,8 +22,8 @@ function initiateLiff(myliffId) {
 
 function initializeApp() {
 
-    // document.getElementById("islogin").textContent = liff.isLoggedIn();
-    // document.getElementById("isclient").textContent = liff.isInClient();
+    document.getElementById("islogin").textContent = liff.isLoggedIn();
+    document.getElementById("isclient").textContent = liff.isInClient();
 
     if (liff.isInClient()) {
         document.getElementById("sendMessage").hidden = false;
@@ -33,7 +33,36 @@ function initializeApp() {
         lineBrowser();
     } else {
         document.getElementById("sendMessage").hidden = true;
-        exBrowser();
+        if (!liff.isLoggedIn()) {
+            document.getElementById("login").addEventListener('click', function () {
+                liff.login();
+            })
+            document.getElementById("logout").addEventListener('click', function () {
+                alert("Silahkan Login Terlebih dahulu");
+            })
+            document.getElementById("getprofile").addEventListener('click', function () {
+                alert("Silahkan Login Terlebih dahulu");
+            })
+        } else {
+            document.getElementById("logout").addEventListener('click', function () {
+                liff.logout();
+                window.location.reload();
+            })
+    
+            document.getElementById("login").addEventListener('click', function () {
+                alert("Anda Sudah Login");
+            })
+    
+            document.getElementById("getprofile").addEventListener('click', function () {
+                liff.getProfile().then(function (profile) {
+                    document.getElementById("userId").textContent = profile.userId;
+                    document.getElementById("displayName").textContent = profile.displayName;
+                    document.getElementById("statusMessage").textContent = profile.statusMessage;
+                })
+            }).catch(function (err) {
+                window.alert("Gagal Mendapatkan info profile " + err);
+            })
+        }
     }
 
 }
